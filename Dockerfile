7c0d3a064b1dcead882e2c3ce9ebb7d78989d011
cybergoka <dgokadze@gmail.com>
2023-03-26 02:47:29 +0400

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#dockerfile for fastapi
+FROM python:3.11-slim-buster
+# set work directory
+WORKDIR /code
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install dependencies
+RUN pip install --upgrade pip
+COPY ./requirements.txt .
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
+# copy project
+COPY . .
+
+# run cmd
+CMD ["uvicorn", "app.main:app"]
+#, "--proxy-headers", "--host", "0.0.0.0", "--port", "8000"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.db import engine
-from app.routers import GameRoute, SymbolRoute, FreeSpinBonusRoute, ReelRoute
+from app.routers import GameRoute, SymbolRoute, FreeSpinBonusRoute, ReelRoute, ChooseBonusRoute
 from app.models import Models
 
 app = FastAPI()
@@ -9,6 +9,7 @@ app = FastAPI()
 app.include_router(GameRoute.router)
 app.include_router(SymbolRoute.router)
 app.include_router(FreeSpinBonusRoute.router)
+app.include_router(ChooseBonusRoute.router)
 app.include_router(ReelRoute.router)
 
 
