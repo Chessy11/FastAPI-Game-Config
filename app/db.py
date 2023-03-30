960695ad813ecdbd06d0cb66377ccac15c49a1c9
cybergoka <dgokadze@gmail.com>
2023-03-30 22:15:00 +0400

    asyncpg problem with sslmode

--- app/db.py
+++ app/db.py
@@ -14,7 +14,7 @@ DATABASE_URL = URL.create(
 )
 
 
-engine = create_async_engine(DATABASE_URL, connect_args={"sslmode": "prefer"})
+engine = create_async_engine(DATABASE_URL, connect_args={"ssl": "prefer"})
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
