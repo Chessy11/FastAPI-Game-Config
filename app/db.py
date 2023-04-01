3bd6e67322343428c78360198c903dc1ef1ee0d0
cybergoka <dgokadze@gmail.com>
2023-04-01 14:56:54 +0400

    connecect_args ssl to require

--- app/db.py
+++ app/db.py
@@ -6,15 +6,15 @@ import os
 
 DATABASE_URL = URL.create(
     drivername="postgresql+asyncpg",
-    username=os.getenv('DB_USER', 'postgres'),
-    password=os.getenv('DB_PASSWORD', 'postgres'),
-    host=os.getenv('DB_HOST', 'localhost'),
-    port=os.getenv('DB_PORT', '5432'),
-    database=os.getenv('DB_NAME', 'gamedb')
+    username=os.getenv('DB_USER', 'conf_db_user'),
+    password=os.getenv('DB_PASSWORD', 'AVNS_C3kJJkHO2hH0gGwtaco'),
+    host=os.getenv('DB_HOST', 'dev-db-all-services-do-user-6051737-0.b.db.ondigitalocean.com'),
+    port=os.getenv('DB_PORT', '25060'),
+    database=os.getenv('DB_NAME', 'conf_db')
 )
 
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, connect_args={"ssl": "require"})
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
